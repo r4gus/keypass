aed7894b9af189d43522b7e2c1b58234f7aad260
David P. Sugar <david@thesugar.de>
2023-11-24 20:56:48 +0100

    more gui function extracted

--- src/gui.zig
+++ src/gui.zig
@@ -1,1 +1,3 @@
 pub const login_frame = @import("gui/login.zig").login_frame;
+pub const info_dialog = @import("gui/info.zig").info_dialog;
+pub const create_db_dialog = @import("gui/create.zig").create_db_dialog;
